3160664c8cebbb2eef158202eef83a1366c9ca36
zhangq002 <zhangq002@gmail.com>
2019-12-17 20:26:18 +0800

    init commit

---
+++ src/main/java/com/ibootz/demomybatis/DemoMybatisApplication.java
@@ -0,0 +1,15 @@
+package com.ibootz.demomybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangq
+ */
+@SpringBootApplication
+public class DemoMybatisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoMybatisApplication.class, args);
+  }
+}
---
+++ src/test/java/com/ibootz/demomybatis/DemoMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibootz.demomybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
